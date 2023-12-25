669017a33f370b4c116013decbc2178c042d09ab
Arnab0089 <arnabkundu756@gmail.com>
2023-12-25 23:27:55 +0530

    FrontEndOnly

---
+++ frontend/src/Components/Main.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './Components-Assets/Main.css'
+import Center from './Components-Assets/Sub-component/center'
+import Header from './Components-Assets/Sub-component/header'
+
+
+export default function Main() {
+
+  return (
+    <>
+    <Header/>
+    <Center/>
+    </>
+  )
+}
